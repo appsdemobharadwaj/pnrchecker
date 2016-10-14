52d2faa9648bfd17c8732efb145b0731dbc3653c
Bharadwaj SNP <appsdemo.bharadwaj@gmail.com>
2016-10-14 19:06:08 +0530

    added support for codedeploy by adding appspec.yml

---
+++ scripts/run_container.sh
@@ -0,0 +1,1 @@
+sudo docker run -d -p 9999:8080 bharadwajsnp/dockerized-war:latest
---
+++ scripts/stop_container.sh
@@ -0,0 +1,2 @@
+sudo docker kill $(sudo docker ps -q -f ancestor="bharadwajsnp/dockerized-war:latest")
+sudo docker rm $(sudo docker ps -a -q -f ancestor="bharadwajsnp/dockerized-war:latest")
