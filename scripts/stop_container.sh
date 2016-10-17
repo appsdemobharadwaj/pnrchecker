fc70aafe93d68a4c404dcee813211a46652272ef
appsdemobharadwaj <appsdemo.bharadwaj@gmail.com>
2016-10-17 15:40:53 +0530

    Modified Stop Sctipt

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,2 +1,3 @@
 sudo docker kill $(sudo docker ps -q -f ancestor="bharadwajsnp/dockerized-war:latest")
 sudo docker rm $(sudo docker ps -a -q -f ancestor="bharadwajsnp/dockerized-war:latest")
+sudo docker rmi bharadwajsnp/dockerized-war:latest
